ef330918318211555028b0dc233b92bba4d9f80b
gabr0 <gabronogue@gmail.com>
2018-04-07 15:41:03 +0200

    msm8996: build SnapdragonCamera

--- msm8996.mk
+++ msm8996.mk
@@ -141,7 +141,7 @@ PRODUCT_PACKAGES += \
     camera.msm8996 \
     libshims_camera \
     libshims_qcamera-daemon \
-    Snap
+    SnapdragonCamera
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
@@ -360,7 +360,7 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
 	thermal.msm8996
-	
+
 	PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
 	$(LOCAL_PATH)/configs/thermal-engine-vr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-vr.conf
