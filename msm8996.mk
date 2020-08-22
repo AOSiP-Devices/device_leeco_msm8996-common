3d45470d26b41d806b08c95ae53aa9fd84cfe78f
AgustinLorenzo <agustin.lorenzo@thinco.es>
2020-08-22 08:39:38 +0200

    Drop the HAL of the touch

--- msm8996.mk
+++ msm8996.mk
@@ -260,10 +260,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.leeco_8996
 
-# Lineage hardware
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.leeco_8996
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
